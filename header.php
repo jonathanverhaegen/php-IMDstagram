8354bc3e9c16362d5409b731be7ea3d512e7431f
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-04-13 14:24:38 +0200

    begin code for header

---
+++ header.php
@@ -0,0 +1,15 @@
+<div class="container-fluid" id="header">
+
+    <div class="row" id="header_row">
+
+        <div class="col">
+            Buckle Up!
+        </div>
+
+    <div class="col">Zoekbalk</div>
+
+    <div class="col">menu</div>
+
+    </div>
+
+</div>
--- userpage.php
+++ userpage.php
@@ -4,17 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/reset.css">
     <link rel="stylesheet" href="style/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Profile</title>
 </head>
 <body>
 
+<?php include_once(__DIR__."/header.php") ?>
+
 <div class="container">
-    <div class="row align-items-start" >
-        <div class="col">test1</div>
-        <div class="col">test2</div>
-        <div class="col">test3</div>
+    <div class="row" >
+        <div class="col-md">test1</div>
+        <div class="col-md">test2</div>
+        <div class="col-md">test3</div>
     </div>
 </div>
 
