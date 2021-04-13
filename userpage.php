c53f79ecd5c1ce704c31aeb59f611b18f2f734b4
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-04-13 14:11:44 +0200

    added userpage and bootstrap

--- userpage.php
+++ userpage.php
@@ -5,9 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <title>Profile</title>
 </head>
 <body>
+
+<div class="container">
+    <div class="row align-items-start" >
+        <div class="col">test1</div>
+        <div class="col">test2</div>
+        <div class="col">test3</div>
+    </div>
+</div>
 
 
     
