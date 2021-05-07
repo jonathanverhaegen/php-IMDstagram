6b0bda4e17b782ff432d1051697f2fb3795def6b
Vinceteckmans <r0737683@student.thomasmore.be>
2021-05-07 20:02:24 +0100

    Background css

--- login.php
+++ login.php
@@ -37,7 +37,7 @@ if(!empty($_POST)){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="styles/style.css">
+	<link rel="stylesheet" href="style/background.css">
     <title>Document</title>
 	
 </head>
