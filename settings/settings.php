22c021d0d1b8944384530793c5fd3f142358755b
Vinceteckmans <r0737683@student.thomasmore.be>
2021-05-16 22:48:52 +0100

    Settings changes

--- settings/settings.php
+++ settings/settings.php
@@ -2,11 +2,11 @@
 
 const SETTINGS = [
     "db" => [
-        "user"      => "root",
-        "password"  => "root",
+        "user"      => "vincezu427_buckleupuser",
+        "password"  => "Buckleup1337",
         "host"      => "localhost",
         "port"      => "3306",
-        "name"      => "buckle_up"
+        "name"      => "vincezu427_buckleup"
     ]
 
     ];
